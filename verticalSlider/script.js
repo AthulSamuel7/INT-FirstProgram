30f9a4ca5e4d3213e8cb6943b49b9efca1b4bd6c
AthulSamuel7 <athulsamuel@gmail.com>
2021-11-05 09:45:57 +0530

    Bouncing ball

--- verticalSlider/script.js
+++ verticalSlider/script.js
@@ -4,24 +4,24 @@ const slideLeft = document.querySelector('.left-slide')
 const upButton = document.querySelector('.up-button')
 const downButton = document.querySelector('.down-button')
 const slidesLength = slideRight.querySelectorAll('div').length
-
+const test = document.write('testing')
 let activeSlideIndex = 0
 
 slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`
 
-upButton.addEventListener('click', () => changeSlide('up'))
-downButton.addEventListener('click', () => changeSlide('down'))
+upButton.addEventListener('click', () => changeSlide('down'))
+downButton.addEventListener('click', () => changeSlide('up'))
 
 const changeSlide = (direction) => {
     const sliderHeight = sliderContainer.clientHeight
-    if(direction === 'up') {
+    if (direction === 'up') {
         activeSlideIndex++
-        if(activeSlideIndex > slidesLength - 1) {
+        if (activeSlideIndex > slidesLength - 1) {
             activeSlideIndex = 0
         }
-    } else if(direction === 'down') {
+    } else if (direction === 'down') {
         activeSlideIndex--
-        if(activeSlideIndex < 0) {
+        if (activeSlideIndex < 0) {
             activeSlideIndex = slidesLength - 1
         }
     }
