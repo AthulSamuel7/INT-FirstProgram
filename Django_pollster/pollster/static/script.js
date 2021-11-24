d2eecd42819e6febc1f5a12c9f560bf7bd16b504
AthulSamuel7 <athulsamuel@gmail.com>
2021-11-24 06:43:29 +0530

    Django Updated

--- Django_pollster/pollster/static/script.js
+++ Django_pollster/pollster/static/script.js
@@ -4,31 +4,9 @@ const excerpt = document.getElementById('excerpt')
 const profile_img = document.getElementById('profile_img')
 const name = document.getElementById('name')
 const date = document.getElementById('date')
+
 const animated_bgs = document.querySelectorAll('.animated-bg')
 const animated_bg_texts = document.querySelectorAll('.animated-bg-text')
-
-
-
-
-
-var mysql = require('mysql');
-
-var con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "1234",
-    database: "library"
-});
-
-// con.connect(function(err) {
-//     if (err) throw err;
-//     console.log("Connected!");
-// });
-var userNameDB = 'test';
-
-
-
-
 
 setTimeout(getData, 2500)
 
@@ -37,26 +15,10 @@ function getData() {
         '<img src="https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80" alt="" />'
     title.innerHTML = 'Lorem ipsum dolor sit amet'
     excerpt.innerHTML =
-        'Tewsting testing'
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore perferendis'
     profile_img.innerHTML =
         '<img src="https://randomuser.me/api/portraits/men/45.jpg" alt="" />'
-
-    con.connect(function(err) {
-        if (err) { throw err }
-        con.query("select Author_name from book_authors", function(err, result, fields) {
-            if (err) throw err;
-            console.log(result);
-            for (let i = 0; i < result.length; i++) {
-                // console.log('Name:', result[i].Author_name)
-                name.innerHTML = result[i].Author_name
-            }
-        });
-    });
-
-
-
-
-    // name.innerHTML = 'John Doe'
+    name.innerHTML = 'John Doe'
     date.innerHTML = 'Oct 08, 2020'
 
     animated_bgs.forEach((bg) => bg.classList.remove('animated-bg'))
